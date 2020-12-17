0e31b02d3f6b985d5a43134af65f53da7c9624ca
GurgenBaghdasaryan <grg.baghdasaryan@gmail.com>
2020-12-17 21:29:10 +0400

    removed comments

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function range(x, y) {
-    // let foltered = x;
     let arr = [];
+
     for (let i = x; i <= y; i++) {
         arr[i - x] = i;
     };
+
     return arr;
 };
 
-console.log(range(10, 10000));
+console.log(range(10, 1000));
