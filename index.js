1eebefcf62d151496bd4ac674da77dfab9766970
GurgenBaghdasaryan <grg.baghdasaryan@gmail.com>
2020-12-18 19:29:46 +0400

    ...

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ let calculator = {
     read(number1, number2) {
         this.Number1 = number1;
         this.Number2 = number2;
-        return this;
     },
 
     sum() {
-        return resultSum = this.Number1 + this.Number2;
+        return this.Number1 + this.Number2;
     },
 
     mul() {
-        return  resultMul = this.Number1 * this.Number2;
+        return this.Number1 * this.Number2;
     }
 }
 
-console.log(calculator.read(12, 25));
+calculator.read(12, 25);
 console.log(calculator.sum());
 console.log(calculator.mul());
