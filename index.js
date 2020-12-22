10f2c6756f4e4fe69812ae75ae713e2cfb0b5dcc
GurgenBaghdasaryan <grg.baghdasaryan@gmail.com>
2020-12-22 23:45:42 +0400

    finished To-Do list

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 let addMessage = document.querySelector('.myInput'),
- addButton = document.querySelector('.add'),
- todo = document.querySelector('.todo');
+    addButton = document.querySelector('.add'),
+    todo = document.querySelector('.todo'),
+    deleteButton = document.querySelector('.delete')
+
 let todoList = [];
 
-addButton.addEventListener('click', function (){
+addButton.addEventListener('click', () => {
     let newTodo = {
         todo: addMessage.value,
-        checked:false,
-        important:false
+        checked: false,
+        important: false,
     };
 
     todoList.push(newTodo);
-    console.log(todoList);
     displayMessages();
 });
 
- function displayMessages(){
+function deleteFromList(index) {
+    todoList.splice(index, 1);
+    displayMessages()
+}
+
+function FromList(i) {
+    document.getElementsByClassName("asd")[i].style.textDecoration = "line-through";
+}
+
+function displayMessages() {
     let displayMessage = '';
-     todoList.forEach(function(item,i) {
-         displayMessage += `
+    todoList.forEach((item, i) => {
+        displayMessage += `
         <li>
-            <input type='checkbox' id='item_${i}'>
-            <label for='item_${i}'>${item.todo}</label>
+            <input type='checkbox'  id='item_${i}' onclick="FromList(${i})">
+            <label for='item_${i}' class='asd' >${item.todo}</label>
+            <button id='item_${i}' onclick="deleteFromList(${i})"></button>
         </li>
         `;
-        todo.innerHTML = displayMessage;
     });
- }
- 
+
+    todo.innerHTML = displayMessage;
+};
+
+deleteButton.addEventListener('click', () => {
+    let newTodo = {
+        todo: deleteButton.value,
+        checked: false,
+        important: false,
+    };
+
+    todoList.splice(newTodo);
+    displayDeleted();
+})
+
+function displayDeleted() {
+    let displayDelete = '';
+    if (todoList.length > 0) {
+        return displayDelete;
+    }
+    todo.innerHTML = displayDelete;
+}
